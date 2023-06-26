9175e08d380a697aaa790ae512087c6c765b7ead
Deepak Mishra <deepmishradm12357@gmail.com>
2023-06-26 23:23:03 +0530

    added favicon

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// sendFile will go here
+app.use('/static', express.static(path.join(__dirname, 'public')))
+
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '/index.html'));
 });
